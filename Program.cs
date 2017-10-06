c384bc245d97d8e0cfae11b586a7731a3b5e9ba8
FIC <caio.chaves.sampaio@gmail.com>
2017-10-06 18:47:24 -0300

    pedir dados

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace Projeto01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string nome, endereco, rg, idade, genero, estadoCivil, cidade, cpf, profissão, localNascimento;
+            nome = PedirDado("Nome");
+            endereco = PedirDado("Endereço");
+            cidade = PedirDado("Cidade");
+            rg = PedirDado("RG");
+            cpf = PedirDado("CPF");
+            estadoCivil = PedirDado("Estado Civil");
+            localNascimento = PedirDado("Local de Nascimento");
+            profissão = PedirDado("Profissão");
+            idade = PedirDado("Idade");
+            genero = PedirDado("Gênero");
+        }
+        static string PedirDado(string dado)
+        {
+            Console.WriteLine(dado + ": ");
+            return Console.ReadLine();
         }
     }
 }
