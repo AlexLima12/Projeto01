ec04a55e12f316dfe7b0c3dbfdac0095f5b7e790
FIC <alexplima@yahoo.com.br>
2017-10-06 19:32:54 -0300

    Mostrar dados concluido

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace Projeto01
     {
         static void Main(string[] args)
         {
+          
+          System.Console.WriteLine( "Digite os dados a seguir");       
             string nome, endereco, rg, idade, genero, estadoCivil, cidade, cpf, profissão, localNascimento;
             nome = PedirDado("Nome");
             endereco = PedirDado("Endereço");
@@ -17,11 +19,31 @@ namespace Projeto01
             profissão = PedirDado("Profissão");
             idade = PedirDado("Idade");
             genero = PedirDado("Gênero");
+
+
+            void MostrarDados()
+            {
+                System.Console.WriteLine("Os dados cadastrados são: ");
+                Console.WriteLine("Nome: " + nome);
+                Console.WriteLine("Endereço: " + endereco);
+                Console.WriteLine("cidade: " + cidade);
+                Console.WriteLine("RG: " + rg);
+                Console.WriteLine("CPF: " + cpf);
+                Console.WriteLine("Estado civil: " + estadoCivil);
+                System.Console.WriteLine("Local de nascimento: " + localNascimento);
+                System.Console.WriteLine("Profissão:" + profissão );
+                System.Console.WriteLine("Idade: " + idade);
+                System.Console.WriteLine("Gênero: " + genero);
+
+            }
+            MostrarDados();
         }
         static string PedirDado(string dado)
         {
             Console.WriteLine(dado + ": ");
             return Console.ReadLine();
         }
+
+        
     }
 }
